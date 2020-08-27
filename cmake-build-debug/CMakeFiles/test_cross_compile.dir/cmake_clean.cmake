7b5a5433f7fdf3133fea845f70c24b5e7419b6bc
Andrey <popov.a83@bk.ru>
2020-08-27 16:11:41 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/test_cross_compile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cross_compile.dir/main.c.o"
+  "test_cross_compile.pdb"
+  "test_cross_compile"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_cross_compile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
